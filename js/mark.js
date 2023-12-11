9631a0fe1837ba254115c8c1ca97f919bca7451c
markgarrettmission <42450552+markgarrettmission@users.noreply.github.com>
2023-12-10 21:13:09 -0800

    Add files via upload

--- js/mark.js
+++ js/mark.js
@@ -4,6 +4,6 @@ onload = function getSize()
 {
     let w = this.document.documentElement.clientWidth;
     let h = this.document.documentElement.clientHeight;
-    this.document.getElementById('wh').innerHTML = "<h1>" +  "width: " + w + "px" + "<br>" + "height: " + h + "px" + "</h1>"
+    this.document.getElementById('wh').innerHTML = "<h1>" +  "<span class='green'> width: " + w + "px </span>" + "<br>" + "height: " + h + "px" + "</h1>"
     window.addEventListener('resize', getSize);
 }
